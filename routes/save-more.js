c24afb353786b9717fa5ff247f7f9696304d4421
Tawfik <yehia.tawfik@vodafone.com>
2019-07-12 17:05:19 +0200

    add calculations

--- routes/save-more.js
+++ routes/save-more.js
@@ -20,7 +20,7 @@ router.get('/save-more', ensureAuthenticated, (req, res) => {
                 res.render('items', {
                     user: req.user,
                     currency: data[1][0],
-                    items:data[0],
+                    items:calculateItemsEndDate(data[0],response[0]),
                     savingDetails:response[0]
                 });
             });
@@ -72,7 +72,7 @@ router.post('/save-items', ensureAuthenticated, (req, res) => {
             res.render('items', {
                 user: req.user,
                 currency: curr[0].code + ' (' + curr[0].symbol_native + ')',
-                items:data[0],
+                items:calculateItemsEndDate(data[0],data[1][0]),
                 savingDetails:data[1][0]
             });
         })
@@ -87,5 +87,79 @@ router.delete('/save-items', ensureAuthenticated,(req, res) => {
         return res.send("Succesfully deleted");
     });
 });
-
+function dhm(t) {
+    var cd = 24 * 60 * 60 * 1000,
+        ch = 60 * 60 * 1000,
+        d = Math.floor(t / cd),
+        h = Math.floor((t - d * cd) / ch),
+        m = Math.round((t - d * cd - h * ch) / 60000),
+        pad = function (n) { return n < 10 ? '0' + n : n; };
+    if (m === 60) {
+        h++;
+        m = 0;
+    }
+    if (h === 24) {
+        d++;
+        h = 0;
+    }
+    return [d, pad(h), pad(m)].join(':');
+}
+function calculateItemPriority(items) {
+    for (var i = 0; i < items.length; i++) {
+        var diff = Math.abs(new Date() - new Date(items[i].date));
+        items[i].daysToBuy = Number(dhm(diff).split(':')[0]);
+    }
+    var sumDays = items.reduce((accumulator, currentValue) => accumulator + currentValue.daysToBuy, 0)
+    for (var j = 0; j < items.length; j++) {
+        items[j].itemPriority = sumDays / items[j].daysToBuy;
+    }
+    var sumPrio = items.reduce((accumulator, currentValue) => accumulator + currentValue.itemPriority, 0)
+    for (var x = 0; x < items.length; x++) {
+        items[x].priorityPerc = (items[x].itemPriority / sumPrio) * 100;
+    }
+    return items;
+}
+function getItemTime(savingDetails, item) {
+    item.savedSoFar = Number(item.savedSoFar) + (item.priorityPerc / 100 * savingDetails.bankAccount);
+    if (savingDetails.bankAccountAfter == undefined) {
+        savingDetails.bankAccountAfter = 0;
+    }
+    if (item.savedSoFar > Number(item.price)) {
+        savingDetails.bankAccountAfter = savingDetails.bankAccountAfter + (item.savedSoFar - item.price);
+        item.savedSoFar -= (item.savedSoFar - item.price);
+    }
+    return item;
+}
+function checkIfThereIsStillItems(items) {
+    let remainingItems = [];
+    for (var x = 0; x < items.length; x++) {
+        if (items[x].savedSoFar != items[x].price) {
+            remainingItems.push(items[x]);
+        }
+    }
+    return remainingItems;
+}
+function calculateItemsEndDate(items, savingDetails) {
+    items = calculateItemPriority(items);
+    for (var x = 0; x < items.length; x++) {
+        items[x] = getItemTime(savingDetails, items[x]);
+    }
+    var remainingItems = checkIfThereIsStillItems(items);
+    if (remainingItems.length) {
+        if (savingDetails.bankAccountAfter) {
+            savingDetails.bankAccount = savingDetails.bankAccountAfter;
+            savingDetails.bankAccountAfter=0;
+        }
+        else {
+            savingDetails.bankAccountAfter = (savingDetails.monthlyIncome - savingDetails.livingExpenses);
+            savingDetails.bankAccount = savingDetails.bankAccountAfter;
+            for (var j = 0; j < items.length; j++) {
+                items[j].remaining = items[j].remaining ?  (items[j].remaining+1) : 1;
+            }
+        }
+        calculateItemsEndDate(remainingItems, savingDetails);
+    }
+    return items;
+console.log(items);
+}
 module.exports = router;
