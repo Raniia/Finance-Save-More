09b433d4fc081188b04ce46d16a4712b6e59ffc4
Mahmoud <rania.mahmoud1@vodafone.com>
2019-07-12 22:01:45 +0200

    fixed names living expenses and monthly bills

--- models/SavingDetails.js
+++ models/SavingDetails.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const savingDetailsSchema = new mongoose.Schema({
     monthlyIncome:{type:String,required:true},
     currencies:{type:String,required:true},
-    livingExpenses:{type:String,required:false},
-    monthlyBills:{type:String,required:true},
+    livingExpenses:{type:String,required:true},
+    monthlyBills:{type:String,required:false},
     bankAccount:{type:String,required:true},
     user: {
         type: mongoose.Schema.Types.ObjectId, ref: 'User'
