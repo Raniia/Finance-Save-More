e78316408da16892954c6b382e175df18d99f13e
Tawfik <yehia.tawfik@vodafone.com>
2019-07-19 16:20:45 +0200

    add countries

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true},
     password:{type:String,required:true},
-    date:{type:Date,default:Date.now}
+    date:{type:Date,default:Date.now},
+    country:{type:String,required:true}
 
 })
 const User = mongoose.model('User',userSchema);
