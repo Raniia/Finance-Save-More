a7996efb7030e575b1789d4d1ee1806b8d09004b
Tawfik <yehia.tawfik@vodafone.com>
2019-07-19 20:29:47 +0200

    fix routes for heroku

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 });
 
 //routes
-app.use("/", require("./routes/index"));
+app.use(require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use(require("./routes/save-more"));
 
